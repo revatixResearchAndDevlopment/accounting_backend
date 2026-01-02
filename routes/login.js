69f29578a8fa200aa7931942c45bd471a9f1ae7c
axyomlabs <axyomlabsrd@gmail.com>
2026-01-02 12:02:24 +0530

    updated login.js

--- routes/login.js
+++ routes/login.js
@@ -24,7 +24,7 @@ app.post("/",async (req, res) => {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
 
-        return res.status(200).json({ message: 'Login successful',  id: user.user_id, username: user.username , name : user.name });
+        return res.status(200).json({success: true, message: 'Login successful',  id: user.user_id, username: user.username , name : user.name });
     } catch (error) {
         console.error('Error during login:', error);
         return res.status(500).json({ message: 'Internal server error' });
