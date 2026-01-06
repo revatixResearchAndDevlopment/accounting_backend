4a84a091bb5c13654d9967f9e38f7729b861b558
axyomlabs <axyomlabsrd@gmail.com>
2026-01-06 16:04:11 +0530

    updateed server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const employee = require("./routes/team_management/employees");
 const products = require("./routes/Products/products");
 const units = require("./routes/Products/units");
 const registration_type = require("./routes/contacts/registration_types");
-const cusstomers = require("./routes/contacts/customers");
+const customers = require("./routes/contacts/customers");
 
 
 app.use("/login", login);
@@ -45,7 +45,7 @@ app.use("/api/employees", employee);
 app.use("/api/products", products);
 app.use("/api/units", units);
 app.use("/api/registration_type", registration_type);
-app.use("/api/cusstomers", cusstomers);
+app.use("/api/customers", customers);
 // Basic Test Route
 app.get("/", (req, res) => {
   res.json({ status: "active", message: "Accounting API is running" });
